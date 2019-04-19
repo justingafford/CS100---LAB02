a2032afca7d36b4812360b0fbf1994cbb81ff9b1
Justin Gafford <jgaff001@ucr.edu>
2019-04-19 02:11:52 -0700

    Updated array.sh and c-echo.cpp to include testing and fixed it to output correct strings

--- c-echo.cpp
+++ c-echo.cpp
@@ -2,8 +2,12 @@
 
 int main(int argv, char** argc) {
     // Skip the first argc index because its the program
-         for(int i = 1; i < argv; i++) {
-                 std::cout << argc[i] << " ";
-          }
-          std::cout << std::endl;
- }
+     for(int i = 1; i < argv; i++) {
+           std::cout << argc[i];
+           // Print a whitespace after all but the last iteration
+           if(i < argv - 1) {
+                  std::cout << " ";
+           }
+     }
+     std::cout << std::endl;
+ }                                                   
